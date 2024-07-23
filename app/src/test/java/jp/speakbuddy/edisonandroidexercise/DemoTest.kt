f331c270b5c9feb32e41ee724fb99b89cde38460
samirshaikh <sksamir09@gmail.com>
2024-07-23 18:15:58 +0530

    Added demo failing test

---
+++ app/src/test/java/jp/speakbuddy/edisonandroidexercise/DemoTest.kt
@@ -0,0 +1,13 @@
+package jp.speakbuddy.edisonandroidexercise
+
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DemoTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(5, 2 + 2)
+    }
+}
